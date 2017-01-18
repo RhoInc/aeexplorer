833bb5a8b5ef8149330b89e05665d7d9fc035675
Jeremy Wildfire <jwildfire@gmail.com>
2017-01-17 17:37:39 -0800

    restructure and document prepareData.js

--- build/aeTable.js
+++ build/aeTable.js
@@ -732,6 +732,15 @@ var aeTable = function () {
 
 	/*------------------------------------------------------------------------------------------------\
    Filter the raw data per the current filter and group selections.
+   After this function is executed there should be 4 data objects bound to the chart:
+   (1) raw_data: an exact copy of the raw data, with an added "placeholderFlag" variable for participants with no events
+   (2) raw_event_data: an exact copy of the raw data with placeholder rows removed
+   (3) population_data: a copy of the raw data filtered by:
+       (a) chart.config.groups - rows from groups not included in the settings object are removed
+       (b) charts.config.variables.filters[type=="participant"] - according to current user selections
+   (4) population_event_data a copy of the population_data with:
+       (a) placeholder rows removed
+       (b) filtered by charts.config.variables.filters[type=="participant"] - according to current user selections
  \------------------------------------------------------------------------------------------------*/
 	function prepareData(chart) {
 		var vars = chart.config.variables; //convenience mapping
@@ -750,6 +759,9 @@ var aeTable = function () {
 				}
 			});
 		});
+		/////////////////////////////////
+		//Create chart.population_data
+		/////////////////////////////////
 
 		//Subset data on groups specified in chart.config.groups.
 		var groupNames = chart.config.groups.map(d => d.key);
@@ -762,6 +774,27 @@ var aeTable = function () {
 				return filter_d.currentValues.indexOf(rowData[filter_d.value_col]) > -1;
 			});
 		});
+
+		///////////////////////////////////////
+		// create chart.population_event_data
+		////////////////////////////////////////
+
+		// Filter event level data
+		chart.population_event_data = chart.population_data.filter(d => !d.placeholderFlag);
+
+		chart.config.variables.filters.filter(function (d) {
+			return d.type == "event";
+		}).forEach(function (filter_d) {
+			//remove the filtered values from the population data
+			chart.population_event_data = chart.population_event_data.filter(function (rowData) {
+				return filter_d.currentValues.indexOf(rowData[filter_d.value_col]) > -1;
+			});
+		});
+
+		////////////////////////////////////////////////////////////////////////
+		// add group-level participant and event counts to chart.config.groups
+		// Used in group headers and to calculate rates
+		////////////////////////////////////////////////////////////////////////
 
 		//Nest data by [vars.group] and [vars.id].
 		var nestedData = d3.nest().key(d => d[vars.group]).key(d => d[vars.id]).entries(chart.population_data);
@@ -776,17 +809,6 @@ var aeTable = function () {
 
 			//Calculate number of events.
 			d.nEvents = chart.raw_data.filter(di => di[vars.group] === d.key && di.placeholderFlag === false).length;
-		});
-
-		//Filter event level data
-		chart.population_event_data = chart.population_data;
-		chart.config.variables.filters.filter(function (d) {
-			return d.type == "event";
-		}).forEach(function (filter_d) {
-			//remove the filtered values from the population data
-			chart.population_event_data = chart.population_event_data.filter(function (rowData) {
-				return filter_d.currentValues.indexOf(rowData[filter_d.value_col]) > -1;
-			});
 		});
 
 		console.log('raw records:' + chart.raw_data.length + ' | raw event records:' + chart.raw_event_data.length + " | pop records: " + chart.population_data.length + " | population event (filtered) records: " + chart.population_event_data.length);
--- src/chart/util/prepareData.js
+++ src/chart/util/prepareData.js
@@ -1,5 +1,14 @@
 /*------------------------------------------------------------------------------------------------\
   Filter the raw data per the current filter and group selections.
+  After this function is executed there should be 4 data objects bound to the chart:
+  (1) raw_data: an exact copy of the raw data, with an added "placeholderFlag" variable for participants with no events
+  (2) raw_event_data: an exact copy of the raw data with placeholder rows removed
+  (3) population_data: a copy of the raw data filtered by:
+      (a) chart.config.groups - rows from groups not included in the settings object are removed
+      (b) charts.config.variables.filters[type=="participant"] - according to current user selections
+  (4) population_event_data a copy of the population_data with:
+      (a) placeholder rows removed
+      (b) filtered by charts.config.variables.filters[type=="participant"] - according to current user selections
 \------------------------------------------------------------------------------------------------*/
 export function prepareData(chart) {
     var vars = chart.config.variables //convenience mapping
@@ -16,7 +25,10 @@ export function prepareData(chart) {
             if (d3.select(this).property('selected')) {filter_d.currentValues.push(option_d)}
         })
     })
-    
+  /////////////////////////////////
+  //Create chart.population_data
+  /////////////////////////////////
+
   //Subset data on groups specified in chart.config.groups.
     var groupNames = chart.config.groups.map(d => d.key);
     chart.population_data = chart.raw_data.filter(d => groupNames.indexOf(d[vars['group']]) >= 0);
@@ -30,6 +42,27 @@ export function prepareData(chart) {
             return filter_d.currentValues.indexOf(rowData[filter_d.value_col])>-1
         }) 
     })
+
+  ///////////////////////////////////////
+  // create chart.population_event_data
+  ////////////////////////////////////////
+
+  // Filter event level data
+  chart.population_event_data = chart.population_data.filter(d => !d.placeholderFlag)
+
+  chart.config.variables.filters.filter(function(d){return d.type=="event"})
+    .forEach(function(filter_d) {
+        //remove the filtered values from the population data
+        chart.population_event_data = chart.population_event_data
+        .filter(function(rowData){
+            return filter_d.currentValues.indexOf(rowData[filter_d.value_col])>-1
+        }) 
+    })
+
+   ////////////////////////////////////////////////////////////////////////
+   // add group-level participant and event counts to chart.config.groups
+   // Used in group headers and to calculate rates
+   ////////////////////////////////////////////////////////////////////////
 
   //Nest data by [vars.group] and [vars.id].
     var nestedData = d3.nest()
@@ -56,17 +89,6 @@ export function prepareData(chart) {
                 .filter(di => di[vars.group] === d.key && di.placeholderFlag === false)
                 .length;
         });
-
-  //Filter event level data
-  chart.population_event_data = chart.population_data
-     chart.config.variables.filters.filter(function(d){return d.type=="event"})
-    .forEach(function(filter_d) {
-        //remove the filtered values from the population data
-        chart.population_event_data = chart.population_event_data
-        .filter(function(rowData){
-            return filter_d.currentValues.indexOf(rowData[filter_d.value_col])>-1
-        }) 
-    })
 
 console.log('raw records:' + chart.raw_data.length + ' | raw event records:' + chart.raw_event_data.length +" | pop records: "+chart.population_data.length +" | population event (filtered) records: "+chart.population_event_data.length)
 
