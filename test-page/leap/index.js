cd7ac182d572a853fc23b472db575475e257e03a
jwildfire <jwildfire@gmail.com>
2019-04-05 06:45:34 -0700

    add leap example

---
+++ test-page/leap/index.js
@@ -0,0 +1,51 @@
+var dataElement = '.graphic-wrapper';
+var dataPath = 'https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/leap-adae1.csv';
+var settings = {
+    variables: {
+        id: 'A_ID',
+        major: 'AESOC',
+        minor: 'AEPT',
+        group: 'TRTC',
+        filters: [
+            {
+                value_col: 'Outcome',
+                label: 'Outcome',
+                type: 'event',
+                start: []
+            },
+            {
+                value_col: 'Treatment Required',
+                label: 'Treatment Required',
+                type: 'event',
+                start: []
+            },
+            {
+                value_col: 'Related?',
+                label: 'Related?',
+                type: 'event',
+                start: []
+            },
+            {
+                value_col: 'Serious?',
+                label: 'Serious?',
+                type: 'event',
+                start: []
+            }
+        ],
+        details: []
+    },
+    groups: [
+        { key: 'Peanut Avoidance', n: 321, selected: true },
+        { key: 'Peanut Consumption', n: 319, selected: true }
+    ],
+    defaults:{
+      placeholderFlag: {
+          value_col: 'SEQNO',
+          values: ['']
+      }}
+};
+
+d3.csv(dataPath, function(error, data) {
+    var instance = aeTable.createChart('#container', settings);
+    instance.init(data);
+});
